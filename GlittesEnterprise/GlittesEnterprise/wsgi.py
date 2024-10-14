9a67a5b95fd6e7e5222e1e47441e8b51241b9297
Caseykingsley77 <caseykingsley77@gmail.com>
2024-10-14 12:59:42 +0100

    wsgi app settings

--- GlittesEnterprise/GlittesEnterprise/wsgi.py
+++ GlittesEnterprise/GlittesEnterprise/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GlittesEnterprise.settings')
 
 application = get_wsgi_application()
+
+app = application
